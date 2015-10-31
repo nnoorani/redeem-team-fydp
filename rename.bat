0ffcb02a4d78aac3dcdc30360367a53527f154e3
whatarthurcodes <arthur.yan3@gmail.com>
2015-10-31 14:49:57 -0400

    batch file to rename pics

---
+++ rename.bat
@@ -0,0 +1,19 @@
+::how to use
+::we took pictures in a systematic order, leverage that and name all the files pic (n)
+::run the batch script and it'll name them to the appropriate name
+
+
+@echo off
+
+ren "pic (1).jpg" "10cm.jpg"
+ren "pic (2).jpg" "25cm.jpg"
+ren "pic (3).jpg" "37.5cm.jpg"
+ren "pic (4).jpg" "50cm.jpg"
+ren "pic (5).jpg" "75cm.jpg"
+ren "pic (6).jpg" "100cm.jpg"
+ren "pic (7).jpg" "30d@25cm.jpg"
+ren "pic (8).jpg" "45d@25cm.jpg"
+ren "pic (9).jpg" "30d@37.5cm.jpg"
+ren "pic (10).jpg" "45d@37.5cm.jpg"
+ren "pic (11).jpg" "30d@50cm.jpg"
+ren "pic (12).jpg" "45d@50cm.jpg"
